a776c2d3b683f0996c34b78cc08e32f546fb70f8
irasema <selmadelgado@gmail.com>
2016-06-21 13:06:47 -0500

    Level 3 Challenges completed

--- app.js
+++ app.js
@@ -27,6 +27,18 @@
   });
  
     
+    app.controller("ReviewController", function(){
+
+    this.review = {};
+
+    this.addReview = function(product){
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+    
+    
   var gems = [
       
     {
